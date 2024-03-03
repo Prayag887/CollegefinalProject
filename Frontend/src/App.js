6fc9f92a1811afa975495abf6068414489e39e15
Prayag887 <prayagdhakal123@gmail.com>
2024-03-03 16:27:36 +0545

    front end final changes

---
+++ Frontend/src/App.js
@@ -0,0 +1,19 @@
+import { BrowserRouter } from 'react-router-dom';
+import Allroutes from './Components/Allroutes';
+import Footer from './Components/Footer';
+import Navbar from './Components/Navbar';
+function App() {
+  return (
+    <div className="App">
+      
+      <Allroutes/>
+     
+    <h1>
+  
+    </h1>
+
+    </div>
+  );
+}
+
+export default App;
